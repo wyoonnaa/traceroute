aef0a29a2b9567969feb700856e8001d7bff1b07
Ёна <114243235+Yonochka@users.noreply.github.com>
2024-05-11 15:43:49 +0500

    Update test1.py
    новые тесты

--- test1.py
+++ test1.py
@@ -1,25 +1,43 @@
+# sudo python3 -m unittest tests.py
+
 import unittest
-from trace import whois, ip_local, traceroute, parse_whois_info
+from trace import is_local_ip, whois_query, traceroute
 
 class TestTracerouteFunctions(unittest.TestCase):
 
-    def test_whois_valid_domain(self):
+    def test_whois_query_valid_domain(self):
         domain_name = "msu.ru"
-        whois_result = whois(domain_name)
-        self.assertIsNotNone(whois_result)
+        whois_result = whois_query(domain_name)
+        self.assertTrue(whois_result.strip())
 
-    def test_ip_local_private_ip(self):
+    def test_is_local_ip_private_ip(self):
         ip_address = "192.168.1.1"
-        result = ip_local(ip_address)
+        result = is_local_ip(ip_address)
         self.assertEqual(result, 'local')
 
-    def test_ip_local_public_ip(self):
+    def test_is_local_ip_public_ip(self):
         ip_address = "8.8.8.8"
-        result = ip_local(ip_address)
+        result = is_local_ip(ip_address)
         self.assertEqual(result, 'no local')
+
+    def test_is_local_ip_invalid_ip(self):
+        invalid_ip = "8474737364oskdk"
+        result = is_local_ip(invalid_ip)
+        self.assertEqual(result, 'invalid')
+
+    def test_is_local_ip_ipv6_local(self):
+        ipv6_local = "2001:0DB8:0000:0000:ABCD::1234"
+        result = is_local_ip(ipv6_local)
+        self.assertEqual(result, 'local')
+
+    def test_is_local_ip_ipv6_random(self):
+        ipv6_random = "ed9a:fe9d:77ed:b4e5:f338:4d34:3b23:3068"
+        result = is_local_ip(ipv6_random)
+        self.assertEqual(result, 'no local')
+
+    def test_traceroute_run(self):
+        self.assertRaises(SystemExit, traceroute, "127.0.0.1")
+
 
 if __name__ == '__main__':
     unittest.main()
-
-# cd /Users/mac/Desktop/python_23_24/task
-# sudo python3 -m unittest tests.py
